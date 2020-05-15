7a2801ae66c1f2babffb9ac4bac960f781e7ffbe
1C0D <63018905+1C0D@users.noreply.github.com>
2020-05-15 05:36:38 +0200

    Added snap on face center

--- rotate_around_active_and _cursor.py
+++ rotate_around_active_and _cursor.py
@@ -32,6 +32,7 @@ import bpy
 from mathutils import Matrix, Euler, Vector
 from bpy.types import Operator, Panel
 from bpy.props import FloatProperty, BoolProperty
+from bpy_extras import view3d_utils
 
 def remove_prop (self,context):  
     
@@ -52,8 +53,8 @@ def remove_prop (self,context):
 Scn = bpy.types.Scene
 Scn.whole_scene=BoolProperty(default=False)
 Scn.around_cursor=BoolProperty(default=False)
-
 ON=False
+r=0
 
 def loc_rot_props(self,context):
     
@@ -119,7 +120,7 @@ def call_props_back(self,context):
 
     context = bpy.context    
     scn = context.scene 
-    
+            
     self.rot = Euler((
             scn.rot_x,
             scn.rot_y,
@@ -143,7 +144,51 @@ def call_props_back(self,context):
     if scn.around_cursor:                 
         self.rot = rot_curs
         self.loc_curs= loc_curs
+        
+def face_to_cursor(self,context):    
   
+    ob=context.object
+    scn = context.scene
+    vert=ob.data.vertices
+
+    if scn.geo: 
+           
+        global r  
+        r=ob.matrix_world  @ (
+        sum((i.co for i in  vert if i.select),Vector())/
+        sum(int(i.select) for i in  vert)) 
+
+        if scn.whole_scene:
+            override = bpy.context.copy()
+            override['selected_objects'] = list(bpy.context.scene.objects)
+            bpy.ops.transform.translate(override, value=(
+            scn.cursor.location.x-r.x, 
+            scn.cursor.location.y-r.y, 
+            scn.cursor.location.z-r.z))
+            
+        else:    
+            bpy.ops.transform.translate(value=(
+            scn.cursor.location.x-r.x, 
+            scn.cursor.location.y-r.y, 
+            scn.cursor.location.z-r.z)) 
+    
+    else:
+
+        if scn.whole_scene:
+            override = bpy.context.copy()
+            override['selected_objects'] = list(bpy.context.scene.objects)
+            bpy.ops.transform.translate(override, value=(
+            -scn.cursor.location.x+r.x, 
+            -scn.cursor.location.y+r.y, 
+            -scn.cursor.location.z+r.z))
+            
+        else:    
+            bpy.ops.transform.translate(value=(
+            -scn.cursor.location.x+r.x, 
+            -scn.cursor.location.y+r.y, 
+            -scn.cursor.location.z+r.z)) 
+               
+Scn.geo=BoolProperty(default=False, update= face_to_cursor)        
 
 class OBJ_OT_rot_loc (Operator):    
     bl_idname = "obj.rot_loc"
@@ -152,7 +197,7 @@ class OBJ_OT_rot_loc (Operator):
     
     @classmethod
     def poll (cls, context):
-        return context.object and not ON and not context.object.parent    #getattr because not rot_x is not enough, can be == 0
+        return context.object and not ON and not context.object.parent   
     
     def execute(self, context): 
         
@@ -160,8 +205,7 @@ class OBJ_OT_rot_loc (Operator):
         ON=True  
         
         context = bpy.context    
-        scn = context.scene 
-         
+        scn = context.scene        
         loc_rot_props(self,context)
         call_props(self,context)
         
@@ -178,8 +222,7 @@ class OBJ_OT_rot_loc (Operator):
             obj = scn.objects
         else:
             obj = context.selected_objects
-                 
-
+        
         for ob in obj:
             if ob.parent:
                 continue
@@ -188,6 +231,7 @@ class OBJ_OT_rot_loc (Operator):
             
             if scn.around_cursor:
                 ob.location +=  self.loc_cur
+                    
             
         return {'FINISHED'}
 
@@ -205,8 +249,8 @@ class OBJ_OT_rot_loc_cancel (Operator):
         global ON
         ON=False  
         context = bpy.context    
-        scn = context.scene          
-
+        scn = context.scene   
+        scn.geo=False     
         call_props_back(self,context)
         
         loc= self.loc
@@ -231,13 +275,24 @@ class OBJ_OT_rot_loc_cancel (Operator):
             
             if scn.around_cursor: 
                 ob.location = M @ (ob.location + loc- self.loc_curs)               
-                    
+                                   
             else:
                 ob.location = M @ (ob.location + loc)
                 
             ob.rotation_euler.rotate(M)
+            
+        if scn.geo:
+            ob=bpy.context.object
+            vert=ob.data.vertices
+            r=ob.matrix_world  @ (
+            sum((i.co for i in  vert if i.select),Vector())/
+            sum(int(i.select) for i in  vert)) 
+            bpy.ops.transform.translate(value=(
+                                -scn.cursor.location.x + r.x,
+                                -scn.cursor.location.y - r.y,
+                                -scn.cursor.location.z - r.z))   
  
-        remove_prop(self,context)
+#        remove_prop(self,context)
 
             
         return {'FINISHED'}
@@ -273,17 +328,100 @@ class OBJ_PT_loc_rot_menu(Panel):
         
         scn = context.scene
         layout = self.layout
-        layout.separator() #to hold the menu to drag it easier
+
         row = layout.row(align=True)
         row.alignment = 'LEFT'   
         row.prop(scn,'whole_scene',text='Scene')
-        row.prop(scn,'around_cursor',text='Cursor')
+        row = layout.row(align=True)
+        row.alignment = 'LEFT'  
+        row.prop(scn,'around_cursor',text='Cursor')  
+        if ON and scn.around_cursor:      
+            row.prop(scn,'geo',text='From selected Geo')
         row=layout.row()
-        label='Cursor'if scn.around_cursor else 'Active'
+        label='Cursor'if scn.around_cursor else 'W Center'
         row.operator("obj.rot_loc",text=label)    
         row.operator("obj.rot_loc_confirm", text='Confirm')
         row.operator("obj.rot_loc_cancel", text='Cancel')
+        layout.separator_spacer
+        layout.operator("view3d.face_center",text="snap Cursor 2 face center")
+
+#-----------------------------snap to face center
+
+
+def main(context, event):
+    
+    scene = context.scene
+    region = context.region
+    rv3d = context.region_data
+    coord = event.mouse_region_x, event.mouse_region_y
+    viewlayer = context.view_layer
+    depsgraph = context.evaluated_depsgraph_get()
+
+    # get the ray from the viewport and mouse
+    view_vector = view3d_utils.region_2d_to_vector_3d(region, rv3d, coord)
+    ray_origin = view3d_utils.region_2d_to_origin_3d(region, rv3d, coord)
+    
+    result, location, normal, index, object, matrix = scene.ray_cast(viewlayer, ray_origin, view_vector)  
+
+    if object:
+        wmtx = object.matrix_world    
+        object_eval = object.evaluated_get(depsgraph)    
         
+        if context.mode=='OBJECT':            
+                        
+            face=object_eval.data.polygons[index]  
+            loc = wmtx @ face.center   
+            
+        else:  
+            mesh_from_eval = bpy.data.meshes.new_from_object(object_eval)
+            
+            face=mesh_from_eval.polygons[index]
+            loc = wmtx @ face.center 
+        
+        bpy.context.scene.cursor.location=loc    
+
+    return {'FINISHED'}
+
+class FACE_OT_center(bpy.types.Operator):
+    bl_idname = "view3d.face_center"
+    bl_label = "snap cursor 2 face center-sfc"     
+    
+    @classmethod
+    def poll(cls, context):
+        return context.mode in {'OBJECT','EDIT_MESH'}
+
+    def modal(self, context, event):        
+        
+        bpy.ops.view3d.cursor3d('INVOKE_DEFAULT',use_depth=False, orientation='GEOM')
+        main(context, event)
+ 
+        if event.type == 'LEFTMOUSE':
+            return {'FINISHED'}
+
+        elif event.type in {'RIGHTMOUSE', 'ESC'}:
+            bpy.context.scene.cursor.location=self.cursor_loc
+            bpy.context.scene.cursor.rotation_euler=self.cursor_rot
+            return {'FINISHED'}
+        
+        elif event.type in {'MIDDLEMOUSE', 'WHEELUPMOUSE', 'WHEELDOWNMOUSE','TAB'}:
+            # allow navigation
+            return {'PASS_THROUGH'}
+
+        return {'RUNNING_MODAL'}
+
+    def invoke(self, context, event):    
+    
+        self.cursor_loc=bpy.context.scene.cursor.location.copy()
+        self.cursor_rot=bpy.context.scene.cursor.rotation_euler.copy()
+        context.window_manager.modal_handler_add(self)
+        return {'RUNNING_MODAL'}        
+
+
+
+
+
+
+#------------------------------        
 
 addon_keymaps = []
 
@@ -292,6 +430,7 @@ def register():
     bpy.utils.register_class(OBJ_OT_rot_loc_cancel)
     bpy.utils.register_class(OBJ_PT_loc_rot_menu)
     bpy.utils.register_class(OBJ_OT_rot_loc_confirm)
+    bpy.utils.register_class(FACE_OT_center)
     
     wm = bpy.context.window_manager
 
@@ -307,6 +446,7 @@ def unregister():
     bpy.utils.unregister_class(OBJ_OT_rot_loc_cancel)
     bpy.utils.unregister_class(OBJ_PT_loc_rot_menu)
     bpy.utils.unregister_class(OBJ_OT_rot_loc_confirm)
+    bpy.utils.unregister_class(FACE_OT_center)
 
     wm = bpy.context.window_manager
     kc = wm.keyconfigs.addon
